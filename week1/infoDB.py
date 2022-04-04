2594982c653303f0877b53fc92501e250c4f09b8
Matthew <pqhantom@gmail.com>
2022-04-03 21:03:02 -0700

    added things to the database and tweaked print data

--- week1/infoDB.py
+++ week1/infoDB.py
@@ -7,45 +7,40 @@ InfoDb.append({
                "Food": "Pizza",  
                "beverage": "water",  
                "sports":["soccer","basketball","tennis","hockey", "Track"]  
-              })  
+              })
 
-InfoDb = []
-# List with dictionary records placed in a list  
-InfoDb.append({ 
-               "Book_Title": "Outliers",  
-               "Author": "Malcolm Gladwell",
-               "Genre": "Nonfiction",
-               "Published": "11/18/2008",  
-               "Author_Books":["The Bomber Mafia", "What the Dog Saw", "Blink", "David and Goliath", "Talking to Strangers"]  
-              })  
-InfoDb.append({  
-               "Book_Title": "Ready Player One",  
-               "Author": "Ernest Cline",
-               "Genre": "Science Fiction",
-               "Published": "8/16/2011",  
-               "Author_Books":["Ready Player Two", "Armada: A Novel", "The Importance of Being Ernest"]   
-              })  
-InfoDb.append({  
-               "Book_Title": "Three Days of Happiness",  
-               "Author": "Sugaru Miaki",
-               "Genre": "Fiction",
-               "Published": "12/25/2013",  
-               "Author_Books":["Your Story", "The Place You Called From", "Parasite in Love", "Starting Over", "Pain, Pain, Go Away"]   
-              })  
-InfoDb.append({  
-               "Book_Title": "No Longer Human",  
-               "Author": "Osamu Dazai",
-               "Genre": "Autobiographical Fiction",
-               "Published": "1948 (No specific date)",  
-               "Author_Books":["The Setting Sun", "Good Bye", "One Hundred Views of Mount Fuji", "The Late Years"]   
-              })  
+InfoDb.append({
+    "Name": "Bob",
+    "School": "Del Norte",
+    "City": "San Diego",
+    "Food": "Egg",
+    "beverage": "water",
+    "sports":["soccer", "Track"]
+})
 
+InfoDb.append({
+    "Name": "Bill",
+    "School": "Del Norte",
+    "City": "San Diego",
+    "Food": "Rice",
+    "beverage": "Lemon",
+    "sports":["basketball","tennis"]
+})
+
+InfoDb.append({
+    "Name": "Jeff",
+    "School": "Del Norte",
+    "City": "San Diego",
+    "Food": "Apple",
+    "beverage": "Coke",
+    "sports":["None"]
+})
 
 def print_data(n):
-    print(InfoDb[n]["Name"], "lives in ", InfoDb[n]["city"])  # using comma puts space between values
+    print(InfoDb[n]["Name"], "lives in ", InfoDb[n]["City"])  # using comma puts space between values
     print("\t", "Food: ", end="")  # \t is a tab indent, end="" make sure no return occurs
     print(", ".join(InfoDb[n]["Food"]))  # join allows printing a string list with separator
-    print("\t", "beverage: ", end="")
+    print("\t", "Beverage: ", end="")
     print(InfoDb[n].get("sports")[0])
     print()
 
